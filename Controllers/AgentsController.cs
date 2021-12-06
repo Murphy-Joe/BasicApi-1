9436cfed767f91faa19be46196e4502abd36c4d5
Jeffry Gonzalez <jeff@hypertheory.com>
2021-12-06 15:06:47 -0500

    Add project files.

---
+++ Controllers/AgentsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicApi.Controllers;
+
+public class AgentsController : ControllerBase
+{
+    [HttpGet("/agents")]
+    public ActionResult<GetAgentsResponse> GetAllAgents()
+    {
+        var response = new GetAgentsResponse();
+        response.Agents = new() { 
+            new AgentResponseItem { Id= 1, FirstName="Bob", LastName="Smith", Email ="bob@aol.com", Phone="555-1212"},
+            new AgentResponseItem { Id= 2, FirstName="Sue", LastName="Jones", Email="sue@aol.com"}
+        };
+        return Ok(response);
+    }
+}
+
+
+public class GetAgentsResponse
+{
+    [Required]
+    public List<AgentResponseItem> Agents { get; set; }  = new List<AgentResponseItem>();
+}
+
+public class AgentResponseItem
+{
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+    [Required]
+    public string Phone { get; set; } = string.Empty;
+    public string? Email { get; set; }
+}
